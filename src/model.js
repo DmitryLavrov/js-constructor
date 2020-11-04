be2e3d33aa5491e04575b71d40b93692afa22459
DmitryLavrov <d3236562@mail.ru>
2020-11-04 20:09:03 +0300

    Добавлены стили для блоков: text, columns, image из исходников

--- src/model.js
+++ src/model.js
@@ -1,5 +1,9 @@
 import image from "./assets/image.png";
 
+const text = `
+Крутые видео и уроки по JavaScript тут: <a href="https://youtube.com/c/VladilenMinin" target="_blank">Владилен Минин</a>. Тут ты найдешь исчерпывающую информацию по любым аспектам языка, любым фреймворкам, такие как: React, Vue, Angular, Node, Svelte, Express, Next, Nuxt и многое другое. Присоединяйся!
+`;
+
 export const model = [
   {
     type: "title",
@@ -15,10 +19,42 @@ export const model = [
       },
     },
   },
-  { type: "text", value: "Some text" },
+  {
+    type: "text",
+    value: text,
+    options: {
+      styles: {
+        background: "linear-gradient(to left, #f2994a, #f2c94c)",
+        padding: "1rem",
+        "font-weight": "bold",
+      },
+    },
+  },
   {
     type: "columns",
-    value: ["1111111", "2222222", "3333333", "444444444", "5555555"],
+    value: [
+      "Приложение на чистом JavaScript, без использования библиотек",
+      "Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс",
+      "JavaScript - это просто, интересно. Научись создавать любые UI своими руками",
+    ],
+    options: {
+      styles: {
+        background: "linear-gradient(to bottom, #8e2de2, #4a00e0)",
+        padding: "2rem",
+        color: "#fff",
+        "font-weight": "bold",
+      },
+    },
   },
-  { type: "image", value: image },
+  {
+    type: "image",
+    value: image,
+    options: {
+      styles: {
+        padding: "2rem 0",
+        display: "flex",
+        "justify-content": "center",
+      },
+    },
+  },
 ];
