57d082aa397b27e81773f5aaaecf64793588af70
DmitryLavrov <d3236562@mail.ru>
2020-11-04 22:50:19 +0300

    В боковой панели выводим HTML-форму с помощью функции block(type)

--- src/classes/sidebar.js
+++ src/classes/sidebar.js
@@ -10,6 +10,21 @@ export class Sidebar {
   }
 
   get template() {
-    return "<h1>template</h1>";
+    return block("text");
   }
+}
+
+export function block(type) {
+  return `
+    <form name="${type}">
+      <h5>${type}</h5>
+        <div class="form-group">
+          <input class="form-control form-control-sm" name="value" placeholder="value">
+        </div>
+        <div class="form-group">
+          <input class="form-control form-control-sm" name="styles" placeholder="styles">
+        </div>
+        <button type="submit" class="btn btp-primary btn-sm">Добавить</button>
+    </form>
+  `;
 }
