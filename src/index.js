af01a9a7183d8395e035ef56acdeb50511b4fabe
DmitryLavrov <d3236562@mail.ru>
2020-11-04 20:45:39 +0300

    Создаем класс Block и используем его в модели

--- src/index.js
+++ src/index.js
@@ -7,18 +7,7 @@ const $site = document.querySelector("#site");
 // console.log(templates["title"]({ value: "text" }));
 
 model.forEach((block) => {
-  // console.log(block);
-  // let html = "";
-  // if (block.type === "title") {
-  //   html = title(block);
-  // } else if (block.type === "text") {
-  //   html = text(block);
-  // } else if (block.type === "columns") {
-  //   html = columns(block);
-  // } else if (block.type === "image") {
-  //   html = image(block);
-  // }
-  // console.log("html: ", html);
+  console.log(block);
 
   const toHTML = templates[block.type];
 
