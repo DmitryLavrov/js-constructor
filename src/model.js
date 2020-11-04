d40726bb6cc71645c049dfea0192671ffd34ac4a
DmitryLavrov <d3236562@mail.ru>
2020-11-04 19:09:28 +0300

    В модель добавили стили. Передаем их в row()

--- src/model.js
+++ src/model.js
@@ -3,9 +3,11 @@ import image from "./assets/image.png";
 export const model = [
   {
     type: "title",
-    value: "Hello World from JS!!!",
+    value: "Конструктор сайтов на чистом JavaScript",
     options: {
       tag: "h2",
+      styles:
+        "background: linear-gradient(to right, #ff0099, #493240);color: #fff; text-align: center; padding: 1.5rem",
     },
   },
   { type: "text", value: "Some text" },
