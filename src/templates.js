6146c04f08d94fb0028adabba82f635cef23a115
DmitryLavrov <d3236562@mail.ru>
2020-11-04 17:56:45 +0300

    Оптимизация: функции для html-блоков в templates.js (модуль utils.js)

--- src/templates.js
+++ src/templates.js
@@ -1,50 +1,21 @@
+import { col, row } from "./utils";
+
 function title(block) {
-  return `
-      <div class="row">
-        <div class="col-sm">
-          <h1>${block.value}</h1>
-        </div>
-      </div>    
-    `;
+  return row(col(`<h1>${block.value}</h1>`));
 }
 
 function text(block) {
-  return `
-      <div class="row">
-        <div class="col-sm">
-          <p>${block.value}</p>
-        </div>
-      </div>
-    `;
+  return row(col(`<p>${block.value}</p>`));
 }
 
 function columns(block) {
-  // let html = "";
-  // block.value.forEach((item) => {
-  //   html += `
-  //       <div class="col-sm">
-  //         <p>${item}</p>
-  //       </div>
-  //   `;
-  // });
+  const html = block.value.map((item) => col(`<p>${item}</p>`));
 
-  const html = block.value.map(
-    (item) => `<div class="col-sm"><p>${item}</p></div>`
-  );
-
-  return `
-      <div class="row">
-        ${html.join("")}
-      </div>
-`;
+  return row(html.join(""));
 }
 
 function image(block) {
-  return `
-  <div class="row">
-    <img src="${block.value}" />
-  </div>
-  `;
+  return row(`<img src="${block.value}" />`);
 }
 
 export const templates = {
