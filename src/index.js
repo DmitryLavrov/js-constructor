074528e30007695fed5ad9cfa63084bf645d7daa
DmitryLavrov <d3236562@mail.ru>
2020-11-04 17:34:31 +0300

    Оптимизация: получаем функцию по ключу из template (имя функции и значение ключа title в модели совпадает)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import { model } from "./model";
-import { title, text, columns, image } from "./templates";
+import { templates } from "./templates";
 import "./styles/main.css";
 
 const $site = document.querySelector("#site");
 
+// console.log(templates["title"]({ value: "text" }));
+
 model.forEach((block) => {
   // console.log(block);
-  let html = "";
-  if (block.type === "title") {
-    html = title(block);
-  } else if (block.type === "text") {
-    html = text(block);
-  } else if (block.type === "columns") {
-    html = columns(block);
-  } else if (block.type === "image") {
-    html = image(block);
-  }
+  // let html = "";
+  // if (block.type === "title") {
+  //   html = title(block);
+  // } else if (block.type === "text") {
+  //   html = text(block);
+  // } else if (block.type === "columns") {
+  //   html = columns(block);
+  // } else if (block.type === "image") {
+  //   html = image(block);
+  // }
   // console.log("html: ", html);
-  $site.insertAdjacentHTML("beforeend", html);
+
+  const toHTML = templates[block.type];
+
+  $site.insertAdjacentHTML("beforeend", toHTML(block));
 });
