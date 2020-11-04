4b7216530ac7fd2c145b120225222c72f83c136d
DmitryLavrov <d3236562@mail.ru>
2020-11-04 18:03:45 +0300

    Импорт картинки в model.js

--- src/model.js
+++ src/model.js
@@ -1,3 +1,5 @@
+import image from "./assets/image.png";
+
 export const model = [
   { type: "title", value: "Hello World from JS!!!" },
   { type: "text", value: "Some text" },
@@ -5,5 +7,5 @@ export const model = [
     type: "columns",
     value: ["1111111", "2222222", "3333333", "444444444", "5555555"],
   },
-  { type: "image", value: "./assets/image.png" },
+  { type: "image", value: image },
 ];
